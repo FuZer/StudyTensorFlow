6de396bc0fc8af6fb84fa932ba9c7d715a03bf11
FuZer <shtowever@gmail.com>
2017-02-10 12:44:12 +0900

    lab 3

---
+++ lab3/hand_made_descent.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+x_data = [1, 2, 3]
+y_data = [1, 2, 3]
+
+W = tf.Variable(tf.random_uniform([1], -10.0, 10.0))
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+hypothesis = W * X
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+lr = 0.1
+descent = W - tf.mul(lr, tf.reduce_mean(tf.mul((tf.mul(W, X) - Y), X)))
+train = W.assign(descent)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(2001):
+    sess.run(train, feed_dict={X: x_data, Y: y_data})
+    if step % 20 == 0:
+        print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W))
+
+print(sess.run(hypothesis, feed_dict={X: 5}))
+print(sess.run(hypothesis, feed_dict={X: 2.5}))
---
+++ lab3/show_cost.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+# Graph Input
+X = [1., 2., 3.]
+Y = [1., 2., 3.]
+m = n_smaples = len(X)
+
+# model weight
+W = tf.placeholder(tf.float32)
+
+# Construct a linear model
+hypothesis = tf.mul(X, W)
+
+# Cost function
+cost = tf.reduce_sum(tf.pow(hypothesis - Y, 2)) / m
+
+init = tf.global_variables_initializer()
+
+# for graphs
+W_val = []
+cost_val = []
+
+# Launch the graphs
+sess = tf.Session()
+sess.run(init)
+
+for i in range(-30, 50):
+    print(i * -0.1, sess.run(cost, feed_dict={W: i * 0.1}))
+    W_val.append(i * 0.1)
+    cost_val.append(sess.run(cost, feed_dict={W: i * 0.1}))
+
+plt.plot(W_val, cost_val, 'ro')
+plt.ylabel('cost')
+plt.xlabel('W')
+plt.show()

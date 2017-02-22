d3a5c780e270b197a59152e0021a3f8441ef2c01
FuZer <shtowever@gmail.com>
2017-02-22 22:26:10 +0900

    lab5

---
+++ lab5/02_ask_to_ml.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+xy = np.loadtxt('train.txt', unpack=True, dtype='float32')
+x_data = xy[0:-1]
+y_data = xy[-1]
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+W = tf.Variable(tf.random_uniform([1, len(x_data)], -1.0, 1.0))
+
+h = tf.matmul(W, X)
+hypothesis = tf.div(1., 1. + tf.exp(-h))
+
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+
+learning_rate = 0.1
+optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+train = optimizer.minimize(cost)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(2001):
+    sess.run(train, feed_dict={X: x_data, Y: y_data})
+
+    if step % 20 == 0:
+        print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W))
+
+print('---------------------------')
+
+print(sess.run(hypothesis, feed_dict={X: [[1], [2], [2]]}) > 0.5)
+print(sess.run(hypothesis, feed_dict={X: [[1], [5], [5]]}) > 0.5)
+print(sess.run(hypothesis, feed_dict={X: [[1, 1], [4, 3], [3, 5]]}) > 0.5)
